857d1510fb6438f5c8b876046f8655b2581555a0
Yan Lyra <yanlyra3@gmail.com>
2022-09-25 18:40:49 -0300

    feat: pass all tests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks((oldTasks) => {
+      if (oldTasks.find((task) => task.title === newTaskTitle)) return oldTasks;
+
+      return [
+        ...oldTasks,
+        {
+          id: oldTasks.length + 1,
+          title: newTaskTitle,
+          done: false,
+        },
+      ];
+    });
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks((oldTasks) => {
+      return oldTasks.map((task) => {
+        if (task.id === id)
+          return {
+            ...task,
+            done: !task.done,
+          };
+
+        return task;
+      });
+    });
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((oldTasks) => oldTasks.filter((task) => task.id !== id));
   }
 
   return (
@@ -26,18 +47,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
